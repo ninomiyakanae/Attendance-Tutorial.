4b0fed287a508f694038181302eb335c89bccfb1
EC2 Default User <ec2-user@ip-172-31-22-189.ap-northeast-1.compute.internal>
2022-11-18 11:49:45 +0000

    Update Gemfile for Heroku

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,6 +4,8 @@ class AttendancesController < ApplicationController
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
 
+# :update_one_month
+
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
   def update
@@ -11,7 +13,7 @@ class AttendancesController < ApplicationController
     @attendance = Attendance.find(params[:id])
     # 出勤時間が未登録であることを判定します。
     if @attendance.started_at.nil?
-      if @attendance.update_attributes(started_at: Time.current.floor_to(15.minutes).change(sec: 0))
+      if @attendance.update_attributes(started_at: Time.current.floor_to(15.minutes).change(sec: 0)) 
         flash[:info] = "おはようございます！"
       else
         flash[:danger] = UPDATE_ERROR_MSG
@@ -30,19 +32,62 @@ class AttendancesController < ApplicationController
   end
 
 
+  # def update_one_month
+  #   ActiveRecord::Base.transaction do # トランザクションを開始します。
+  #     attendances_params.each do |id, item|
+  #       attendance = Attendance.find(id)
+  #       attendance.update_attributes!(item)
+  #     end
+  #   end
+  #   flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+  #   redirect_to user_url(date: params[:date])
+  # rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+  #   flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+  #   redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  # end
+  
+  
   def update_one_month
-    ActiveRecord::Base.transaction do # トランザクションを開始します。
-      attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
+      ActiveRecord::Base.transaction do
+        attendances_params.each do |id, item|
+          # attendance = Attendance.find(id)
+          if item[:started_at].present? && item[:finished_at].blank?
+            flash[:danger] = "退勤も更新してください"
+            redirect_to attendances_edit_one_month_user_url(date: params[:date])and return
+          # else
+          end  
+            # attendance = Attendance.find(id)
+            # attendance.update_attributes!(item)
+            attendance = Attendance.find(id)
+            attendance.update_attributes!(item)
+        end
       end
-    end
-    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
-  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
-    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
-    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+      flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+      redirect_to user_url(date: params[:date])and return
+  rescue ActiveRecord::RecordInvalid
+      flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+      redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
+  
+# def update_one_month
+#     ActiveRecord::Base.transaction do
+#       attendances_params.each do |id, item|
+#         attendance = Attendance.find(id)
+#         if item[:started_at].present? && item[:finished_at].blank?
+#           flash[:danger] = "退勤も更新してください"
+#           redirect_to attendances_edit_one_month_user_url(date: params[:date])and return
+#         else
+#           attendance.update_attributes!(item)
+#           flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+#           redirect_to user_url(date: params[:date])and return
+#         end
+#       end
+#     end
+#   rescue ActiveRecord::RecordInvalid
+#     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+#     redirect_to attendances_edit_one_month_user_url(date: params[:date])
+#   end
+  
 
   private
 
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,6 +6,8 @@ class Attendance < ApplicationRecord
 
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
+  # 退勤時間が存在しない場合、出勤時間は無効
+  # validate :started_at_is_invalid_without_a_finished_at
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
 
